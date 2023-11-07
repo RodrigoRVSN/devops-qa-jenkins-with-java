ca41c876a1268ead7441b16c2d699d2dfe827b0f
Charles Oliveira <105311867+OliveiraCharles@users.noreply.github.com>
2023-11-07 19:15:45 -0300

    adicionado endpoint /{aluno_id}/adquire-curso/{curso_id}

--- script/run.sh
+++ script/run.sh
@@ -1,1 +1,2 @@
+# mvn clean && \
 mvn spring-boot:run # -e -X
--- src/main/java/com/facens/ac2/model/Curso.java
+++ src/main/java/com/facens/ac2/model/Curso.java
@@ -27,18 +27,49 @@ public class Curso {
   private double nota02 = 0.0;
   private double notaFinal = 0.0;
   private double media = 0.0;
+  private double frequencia = 1.0;
 
   public Curso(String name) {
     this.name = name;
   }
 
   public boolean estaAprovado() {
-    double media = calcularMedia();
-    return media >= 7.5;
+    double media;
+    if (frequencia < 75) {
+      return false;
+    } else {
+      media = (nota01 + nota02) / 2;
+      // System.out.println("Media: " + media);
+      if (media < 30) {
+        // System.out.println("Reprovado: Media < 30!" + media);
+        return false;
+      } else {
+        if (media >= 70) {
+          // System.out.println("Aprovado: Media >= 70!");
+          return true;
+        } else {
+          if ((media + notaFinal) / 2 >= 50) {
+            // System.out.println("Aprovado: Media2 >= 50!");
+            return true;
+          } else {
+            // System.out.println("Reprovado: Media2 < 50!");
+            return false;
+          }
+        }
+      }
+    }
   }
 
-  public double calcularMedia() {
-    this.media = (this.nota01 + this.nota02 + this.notaFinal) / 3;
-    return this.media;
+  public void show() {
+    System.out.println("Nome: " + getName());
+    System.out.println("\tFrequência:\t " + getFrequencia() * 100 + "%");
+    System.out.println("\tNota 01:\t " + getNota01());
+    System.out.println("\tNota 02:\t " + getNota02());
+    System.out.println("\tNota Final:\t " + getNotaFinal());
+    if (estaAprovado()) {
+      System.out.println("\tAprovado");
+    } else {
+      System.out.println("\tReprovado");
+    }
   }
 }
